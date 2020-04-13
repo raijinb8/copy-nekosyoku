9dc5282799d6b4868256e9887471603e3945be49
raijinb8 <ranjinb8@gmail.com>
2020-04-14 01:17:08 +0900

    Create top.js

---
+++ js/top.js
@@ -0,0 +1,51 @@
+// var mySwiper = new Swiper('.swiper-container', {
+//   slidesPerView: 2,
+//   speed: 1000,
+//   loop: true,
+// 	autoplay: {
+// 		delay: 3000,
+// 		stopOnLastSlide: false,
+// 		disableOnInteraction: false,
+// 		reverseDirection: false
+// 	}
+// });
+
+//1個目のSwiper
+var mySwiper1 = new Swiper('.swiper1', {
+  speed: 1000,
+  loop: true,
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	}
+});
+
+//2個目のSwiper
+var mySwiper2 = new Swiper('.swiper2', {
+  direction: 'vertical',
+  effect: 'slide',
+  speed: 1000,
+  loop: true,
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	}
+});
+
+/* 一覧ホバー設定
+	-----------------------------------------------------------------*/
+	$('.fade').hover(function() {
+		$(this).fadeTo("fast",0.8);
+	}, function() {
+		$(this).fadeTo("fast",1.0);
+	});
+
+	/* Gナビ設定
+	-----------------------------------------------------------------*/
+$('#gnaviBtn').on('click', function() {
+	$('body').toggleClass('opened');
+});
