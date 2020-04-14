22d763c4aa9b9a36537c28b81e3bbcc9fcdfbfd0
raijinb8 <ranjinb8@gmail.com>
2020-04-14 15:43:37 +0900

    update top.js

--- js/top.js
+++ js/top.js
@@ -1,15 +1,3 @@
-// var mySwiper = new Swiper('.swiper-container', {
-//   slidesPerView: 2,
-//   speed: 1000,
-//   loop: true,
-// 	autoplay: {
-// 		delay: 3000,
-// 		stopOnLastSlide: false,
-// 		disableOnInteraction: false,
-// 		reverseDirection: false
-// 	}
-// });
-
 //1個目のSwiper
 var mySwiper1 = new Swiper('.swiper1', {
   speed: 1000,
@@ -38,14 +26,16 @@ var mySwiper2 = new Swiper('.swiper2', {
 
 /* 一覧ホバー設定
 	-----------------------------------------------------------------*/
-	$('.fade').hover(function() {
-		$(this).fadeTo("fast",0.8);
-	}, function() {
-		$(this).fadeTo("fast",1.0);
-	});
+	// $('.fade').hover(function() {
+	// 	$(this).fadeTo("fast",0.8);
+	// }, function() {
+	// 	$(this).fadeTo("fast",1.0);
+	// });
 
 	/* Gナビ設定
 	-----------------------------------------------------------------*/
-$('#gnaviBtn').on('click', function() {
-	$('body').toggleClass('opened');
-});
+	$('.menu').on('click',function(){
+		$('.menu__line').toggleClass('active');
+		$('#gnav').fadeToggle();
+	});
+	
